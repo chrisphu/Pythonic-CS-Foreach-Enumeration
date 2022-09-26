3d903a1fea8e40b74161349c9a99cba3609a7c87
Chris Phu <67293666+chrisphu@users.noreply.github.com>
2022-09-25 22:46:04 -0500

    Create Utils.cs

---
+++ Utils.cs
@@ -0,0 +1,15 @@
+using System.Linq;
+
+public static class Utils
+{
+	// C# 7.0 and later, otherwise use Tuple<int, T> and Tuple.Create()
+	public static (int, T)[] Enumerate<T>(this T[] values)
+	{
+		(int, T)[] indexesValues = new (int, T)[values.Count()];
+		for (int index = 0; index < values.Count(); index++)
+		{
+			ivs[index] = (index, values[index]);
+		}
+		return indexesValues;
+	}
+}
